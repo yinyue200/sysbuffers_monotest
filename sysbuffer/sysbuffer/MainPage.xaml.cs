0d0cf51246de97dda33dd9c07bec5d7a911c2aed
yinyue200 <yinyue200@outlook.com>
2017-12-16 09:13:31 +0800

    添加项目文件。

---
+++ sysbuffer/sysbuffer/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sysbuffer
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent(); ;
+            if (System.Buffers.ArrayPool<byte>.Shared.Rent(10).Length < 10)
+            {
+                throw new Exception();
+            }
+		}
+	}
+}
